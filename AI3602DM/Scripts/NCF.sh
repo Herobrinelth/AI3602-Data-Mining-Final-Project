d32a079ab1ef859bcb82a1a2d7441644cf471caa
Tianhua Li <109146676+Herobrinelth@users.noreply.github.com>
2024-05-22 00:12:25 +0800

    Add files via upload

---
+++ AI3602DM/Scripts/NCF.sh
@@ -0,0 +1,8 @@
+echo "Data preprocessing"
+python Tools/Dataset.py
+
+echo "Begin to train NCF model..."
+python Method/NCF_train.py
+
+echo "Recommending by NCF method..."
+python Method/NCF_recommand.py
